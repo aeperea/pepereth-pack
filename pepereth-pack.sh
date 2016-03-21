cdf254ccb4bcfc25d9f90681efa2436a2df08757
aeperea <elizondoperea@gmail.com>
2016-03-21 13:36:47 -0600

    Add tag message for indication

--- pepereth-pack.sh
+++ pepereth-pack.sh
@@ -28,8 +28,8 @@ linux_repos=(
 
 osx_installer ()
 {
-    echo "Installing Peperth Pack for OSX"
-    echo "(might need password for package installation)"
+    echo "[PEPERETH PACK] -----> Installing Peperth Pack for OSX"
+    echo "[PEPERETH PACK] -----> (might need password for package installation)"
 
     xcode-select --install
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -37,13 +37,13 @@ osx_installer ()
     curl -sSL https://get.rvm.io | bash -s stable --ruby
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 
-    echo "Installing repositories"
+    echo "[PEPERETH PACK] -----> Installing repositories"
     for i in "${brew_repos[@]}"
     do
         brew tap $i
     done
 
-    echo "Intstalling packages"
+    echo "[PEPERETH PACK] -----> Intstalling packages"
     for i in "${package_list[@]}"
     do
         brew install $i
@@ -55,17 +55,17 @@ osx_installer ()
 
 linux_installer ()
 {
-    echo "Installing Peperth Pack for Linux Debian"
-    echo "(might need password for package installation)"
+    echo "[PEPERETH PACK] -----> Installing Peperth Pack for Linux Debian"
+    echo "[PEPERETH PACK] -----> (might need password for package installation)"
     sudo apt-get update
 
-    echo "Installing repositories"
+    echo "[PEPERETH PACK] -----> Installing repositories"
     for i in "${linux_repos[@]}"
     do
 	      sudo apt-add-repository $i
     done
 
-    echo "Intstalling packages"
+    echo "[PEPERETH PACK] -----> Intstalling packages"
     for i in "${package_list[@]}"
     do
 	      sudo apt-get install -y $i
